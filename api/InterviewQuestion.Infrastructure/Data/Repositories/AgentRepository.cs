8311f9a94667b26f26eb73a75d564354a466dfad
matthew.idowu <matthew.idowu@bankly.ng>
2022-10-24 16:06:48 +0100

    added agent repository

---
+++ api/InterviewQuestion.Infrastructure/Data/Repositories/AgentRepository.cs
@@ -0,0 +1,21 @@
+﻿using InterviewQuestion.Domain.Entities;
+using InterviewQuestion.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestion.Infrastructure.Data.Repositories
+{
+    public interface IAgentRepository:IRepositoryAsync<Agent>
+    {
+
+    }
+    public class AgentRepository : RepositoryAsync<Agent>, IAgentRepository
+    {
+        public AgentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
